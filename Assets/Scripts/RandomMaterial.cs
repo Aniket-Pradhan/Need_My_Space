c7e8d73445169a61afe113021e3bb20d46d1f562
Aniket Pradhan <aniket17133@iiitd.ac.in>
2018-09-29 10:50:33 +0530

    Added main character

--- Assets/Scripts/RandomMaterial.cs
+++ Assets/Scripts/RandomMaterial.cs
@@ -16,20 +16,21 @@ public class RandomMaterial : MonoBehaviour {
     /// <returns></returns>
     public Material GetRandomMaterial()
     {
+        return Resources.Load("Materials/whiteMaterial") as Material;
         int x = 0;
 //        int x = Random.Range(0, 5);
-        if (x == 0)
-            return Resources.Load("Materials/redMaterial") as Material;
-        else if (x == 1)
-            return Resources.Load("Materials/greenMaterial") as Material;
-        else if (x == 2)
-            return Resources.Load("Materials/blueMaterial") as Material;
-        else if (x == 3)
-            return Resources.Load("Materials/yellowMaterial") as Material;
-        else if (x == 4)
-            return Resources.Load("Materials/purpleMaterial") as Material;
-        else
-            return Resources.Load("Materials/redMaterial") as Material;
+//        if (x == 0)
+//            return Resources.Load("Materials/redMaterial") as Material;
+//        else if (x == 1)
+//            return Resources.Load("Materials/greenMaterial") as Material;
+//        else if (x == 2)
+//            return Resources.Load("Materials/blueMaterial") as Material;
+//        else if (x == 3)
+//            return Resources.Load("Materials/yellowMaterial") as Material;
+//        else if (x == 4)
+//            return Resources.Load("Materials/purpleMaterial") as Material;
+//        else
+//            return Resources.Load("Materials/redMaterial") as Material;
     }
 
 }
